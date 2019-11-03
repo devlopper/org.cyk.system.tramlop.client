1a9647022bb21451b39676acf953b3fa298136df
Christian Yao Komenan <kycdev@gmail.com>
2019-11-03 17:10:41 +0000

    commit

--- tramlop-client-controller-entities/src/main/java/org/cyk/system/tramlop/client/controller/entities/product/ProductWeightMovement.java
+++ tramlop-client-controller-entities/src/main/java/org/cyk/system/tramlop/client/controller/entities/product/ProductWeightMovement.java
@@ -5,9 +5,12 @@ import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 
 import org.cyk.utility.client.controller.component.annotation.Input;
+import org.cyk.utility.client.controller.component.annotation.InputChoice;
+import org.cyk.utility.client.controller.component.annotation.InputChoiceOne;
+import org.cyk.utility.client.controller.component.annotation.InputChoiceOneCombo;
 import org.cyk.utility.client.controller.component.annotation.InputString;
 import org.cyk.utility.client.controller.component.annotation.InputStringLineOne;
-import org.cyk.utility.client.controller.data.AbstractDataIdentifiableSystemStringIdentifiableBusinessStringNamableImpl;
+import org.cyk.utility.client.controller.data.AbstractDataIdentifiableSystemStringIdentifiableBusinessStringImpl;
 import org.cyk.utility.client.controller.data.Data;
 
 import lombok.Getter;
@@ -15,14 +18,18 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Getter @Setter @Accessors(chain=true)
-public class Product extends AbstractDataIdentifiableSystemStringIdentifiableBusinessStringNamableImpl implements Data,Serializable {
+public class ProductWeightMovement extends AbstractDataIdentifiableSystemStringIdentifiableBusinessStringImpl implements Data,Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Input @InputChoice @InputChoiceOne @InputChoiceOneCombo
+	@NotNull
+	private Product product;
+	
 	@Input @InputString @InputStringLineOne
 	@NotNull
 	private String weight;
 
 	/**/
-	
+	public static final String FIELD_PRODUCT = "product";
 	public static final String FIELD_WEIGHT = "weight";
 }

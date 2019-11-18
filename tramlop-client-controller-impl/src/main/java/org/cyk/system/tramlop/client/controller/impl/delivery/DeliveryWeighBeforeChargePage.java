a7e67cdbaf5f9ff53fe6b7a29a114437ac660756
Christian Yao Komenan <kycdev@gmail.com>
2019-11-18 22:49:54 +0000

    commit

---
+++ tramlop-client-controller-impl/src/main/java/org/cyk/system/tramlop/client/controller/impl/delivery/DeliveryWeighBeforeChargePage.java
@@ -0,0 +1,30 @@
+package org.cyk.system.tramlop.client.controller.impl.delivery;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.cyk.system.tramlop.client.controller.api.TruckController;
+import org.cyk.system.tramlop.server.persistence.api.TruckPersistence;
+import org.cyk.utility.__kernel__.properties.Properties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Named @ViewScoped @Getter @Setter
+public class DeliveryWeighBeforeChargePage extends AbstractDeliveryPage implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public void create() {
+		trucks = __inject__(TruckController.class).read(new Properties()
+				.setQueryIdentifier(TruckPersistence.READ_WHERE_AGREEMENT_CLOSED_IS_FALSE_EXIST_AND_DELIVERY_CLOSED_IS_FALSE_DOES_NOT_EXIST)
+				.setIsQueryResultPaginated(Boolean.FALSE));
+	}
+	
+	@Override
+	protected String __getWindowTitleValue__() {
+		return "Pesée de camion vide avant chargement";
+	}
+	
+}
